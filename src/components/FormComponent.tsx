be76fcc8b2b5b14d6b52624cfe87e1c5a12086d6
ReedS <ReedStock1992@gmail.com>
2021-04-19 22:06:42 -0700

    Initial setup

---
+++ src/components/FormComponent.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+interface IFormProps {
+    
+}
+
+export default function FormComponent(props: IFormProps){
+    return (
+        <div>
+            Hello Form
+        </div>
+    )
+}
